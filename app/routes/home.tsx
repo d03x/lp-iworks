22d5038e598df47a62d4a6d68c248e687b29708c
devichr <devianoananda@gmail.com>
2025-06-03 09:09:40 +0700

    change title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "iWorks : Digital Informal worker Platforms" },
+    { name: "description", content: "iWorks is a digital platform for informal workers to find job and oppurtunity" },
   ];
 }
 
